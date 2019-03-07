225fe57939ea171d1b111e945e61459dcc78bd02
Haro Orozco <haroorozco11103@hotmail.com>
2019-03-07 16:11:32 -0600

    Modificar registros

--- app/Http/Controllers/JornadaController.php
+++ app/Http/Controllers/JornadaController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+//use Illuminate\Support\Facades\DB;
+use App\Jornada;
 use Illuminate\Http\Request;
 
 class JornadaController extends Controller
 {
     public function index()
     {
-      $qniela = DB::table('jornadas')->get();
+      $qniela = Jornada::all();
+      //$qniela = Jornada::where('id', '>', '1')->get();
+
+      //$qniela = DB::table('jornadas')->get();
       //dd($qniela);
       //return $qniela;
       return view('jornadas.jornadaIndex', compact('qniela'));
---
+++ app/Jornada.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jornada extends Model
+{
+    //protected $table = 'nombre'; //relaciona modelo con esa tabla por si la clase se llama diferente en plural o algo así
+}
