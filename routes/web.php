baf2b161addee22df6c0422bf0d24f7a72a0d382
Haro Orozco <haroorozco11103@hotmail.com>
2019-05-05 18:56:26 -0500

    Aplicar middleware de autenticación de usuario loggeado a rutas.

--- routes/web.php
+++ routes/web.php
@@ -23,36 +23,36 @@ Route::get('/inicio', function ()
 
 //--------------------------------------------------------USERS--------------------------------------------------------
 //Route::get('/usuarios', 'UserController@index')->name('users.index');//->middleware('auth');   //Vreificación de login (retorna a la pagina de log)
-Route::resource('users', 'UserController');
+Route::resource('users', 'UserController')->middleware('auth');
 Route::get('users/contrasena/{user}', 'UserController@editPass')
-->name('users.editPass');
+->name('users.editPass')->middleware('auth');
 Route::PATCH('users/cambiar-contraseña/{user}', 'UserController@updatePass')
-->name('users.updatePass');
+->name('users.updatePass')->middleware('auth');
 
 //-------------------------------------------------------EQUIPOS-------------------------------------------------------
 //Route::get('/equipo', 'EquipoController@index')->name('equipos.index');
-Route::resource('equipos', 'EquipoController');
+Route::resource('equipos', 'EquipoController')->middleware('auth');
 //-----------------------------------------------------PRONOSTICOS-----------------------------------------------------
 //Route::get('/pronosticos', 'PronosticoController@index')->name('pronosticos.index');
-Route::resource('pronosticos', 'PronosticoController');
+Route::resource('pronosticos', 'PronosticoController')->middleware('auth');
 
 //-------------------------------------------------------PARTIDOS------------------------------------------------------
 //Route::get('/resultados', 'PartidoController@index')->name('partidos.index');
-Route::resource('partidos', 'partidoController');
+Route::resource('partidos', 'partidoController')->middleware('auth');
 Route::get('/partidos/partidos-por-jornada/{jornada}', 'PartidoController@showJorn')
-    ->name('partidos.showJorn');
+    ->name('partidos.showJorn')->middleware('auth');
 Route::get('/partidos/partidos-resultados/{jornada}', 'PartidoController@editRes')
-    ->name('partidos.editRes');
+    ->name('partidos.editRes')->middleware('auth');
 Route::get('/partidos/create/{jornada?}', 'PartidoController@createParJorX')
-    ->name('partidos.createParJorX');
+    ->name('partidos.createParJorX')->middleware('auth');
 //Route::match(['put', 'patch'], '/partidos/cambiar-resultados', 'PartidoController@updateRes')
     //->name('partidos.updateRes');
 Route::PATCH('/partidos/cambiar-resultados/{partido}', 'PartidoController@updateRes')
-    ->name('partidos.updateRes');
+    ->name('partidos.updateRes')->middleware('auth');
 
 //------------------------------------------------------JORNADAS-------------------------------------------------------
 //Route::get('/jornadas', 'JornadaController@index')->name('jornadas.index');
-Route::resource('jornadas', 'JornadaController');
+Route::resource('jornadas', 'JornadaController')->middleware('auth');
 
 //-------------------------------------------------------PAGINAS-------------------------------------------------------
 Route::get('/informacion', 'PaginasController@info');
